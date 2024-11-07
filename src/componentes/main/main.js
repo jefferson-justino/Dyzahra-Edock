a27e6606c6ee92fd6912734924f947cc51b7572a
Jeff <itachisucan@gmail.com>
2024-11-07 15:32:54 -0300

    adição de cores

--- src/componentes/main/main.js
+++ src/componentes/main/main.js
@@ -1,5 +1,5 @@
 import imgYoutube from '../../icones/youtube.svg'
-// import imgPix from '../../icones/livepix.svg'
+import imgKawai from '../../icones/kawaii.png'
 import imgInstagram from '../../icones/instagram.svg'
 import imgTiktok from '../../icones/tik-tok.svg'
 import imgDiscord from '../../icones/discord.svg'
@@ -10,10 +10,12 @@ import './main.css'
 export default function Main (){
     const objetos = [
         {icone: imgYoutube, titulo: 'Youtube', link:'https://www.youtube.com/@dyzahra_edock' },
-        {icone: imgDiscord, titulo: 'Discord', link:'https://discord.gg/Jt88jVeN' },
-        {icone: imgInstagram, titulo: 'Instagram', link:'https://www.instagram.com/dyzahra_edock' },
-        {icone: imgTiktok, titulo: 'TikTok', link:'https://www.tiktok.com/@dyzahra_edock?' },
+        {icone: imgDiscord, titulo: 'Discord', link:'https://discord.gg/tTJZadTpC3' },
         {icone: imgThreads, titulo: 'threads', link:'https://www.threads.net/@dyzahra_edock' },
+        {icone: imgInstagram, titulo: 'Instagram', link:'https://www.instagram.com/dyzahra_edock' },
+        {icone: imgKawai, titulo: 'Kwai', link:'https://www.kwai.com/@Dyzahra_Edock' },
+        {icone: imgTiktok, titulo: 'TikTok', link:'https://www.tiktok.com/@dyzahra_edock?' },
+        
     ]
 
     let conteudo = objetos.map((item)=>{
